31dde21ab36311f3072c0b44a9baa9d46b6dd16c
Xuemin Sun <XueminSun@china-liantong.com>
2016-04-25 18:58:56 +0800

    modify MappingType and WritePolicy

--- base.cpp
+++ base.cpp
@@ -3,33 +3,34 @@
 //
 #include <stdio.h>
 #include "base.h"
+#include "cache.h"
 
 using namespace base;
 
-string base::result2string(result &r) {
+string base::result2string(const result &r) {
     string mapping_type;
     string write_policy;
     switch (r.mapping_type) {
-        case MappingType::MT_DM:
+        case MT_DM:
             mapping_type += "DM";
             break;
-        case MappingType::MT_2W:
+        case MT_2W:
             mapping_type += "2W";
             break;
-        case MappingType::MT_4W:
+        case MT_4W:
             mapping_type += "4W";
             break;
-        case MappingType::MT_FA:
+        case MT_FA:
             mapping_type += "FA";
             break;
         default:
             break;
     }
     switch (r.write_policy) {
-        case WritePolicy::WP_WB:
+        case WP_WB:
             write_policy += "WB";
             break;
-        case WritePolicy::WP_WT :
+        case WP_WT :
             write_policy += "WT";
             break;
         default:
@@ -39,14 +40,15 @@ string base::result2string(result &r) {
     char temp[100];
     sprintf(temp, "%d\t%d\t%s\t%s\t%.2f\t%d\t%d\t%d", r.cache_size, r.block_size, mapping_type.c_str(),
             write_policy.c_str(), r.hit_rate, r.mem2cache,
-            r.cache2mem, r.cmp_counter);
+            r.cache2mem, r.cmp_count);
     return string(temp);
 }
 
-int base::hex2int(const char *hex) {
-    int value = 0;
+unsigned int base::hex2int(const char *hex) {
+    unsigned int value = 0;
     sscanf(hex, "%x", &value);
     return value;
 }
+
 
 
--- base.h
+++ base.h
@@ -8,16 +8,17 @@
 #include <string>
 
 using std::string;
+class cache;
 namespace base {
     enum MappingType {
-        MT_DM = 0,
-        MT_2W,
-        MT_4W,
-        MT_FA,
+        MT_DM = 1,
+        MT_2W = 2,
+        MT_4W = 4,
+        MT_FA = 8,
     };
     enum WritePolicy {
-        WP_WB = 0,
-        WP_WT,
+        WP_WT = 0,
+        WP_WB = 1,
     };
 
 
@@ -29,7 +30,7 @@ namespace base {
         double hit_rate;
         int mem2cache;
         int cache2mem;
-        int cmp_counter;   //验证是否命中cache的比较次数。
+        int cmp_count;   //验证是否命中cache的比较次数。
         // 映射方式为直接映射(MT_DM)，比较次数为1；映射方式为二路相联(MT_2W)，比较次数为2；
         // 映射方式为四路相联(MT_4W)，比较次数为4；映射方式为全相联(MT_FA)，比较次数为cache_size/block_size；
     } result;
@@ -37,14 +38,12 @@ namespace base {
     /*
      * 将result转为字符串输出
      */
-    string result2string(result &r);
+    string result2string(const result &r);
 
     /*
     * 功能：将十六进制字符串转换为整型(int)数值
     */
-    int hex2int(const char *hex);
-
-
+    unsigned int hex2int(const char *hex);
 }
 
 #endif //CACHE_SIMULATOR_RESULT_H
