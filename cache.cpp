1c6aa7a39d76451f60702c49a3db1003c7682519
Xuemin Sun <XueminSun@china-liantong.com>
2016-04-26 16:52:24 +0800

    add hex2int function, finx bug in the process function

--- cache.cpp
+++ cache.cpp
@@ -4,28 +4,32 @@
 #include <vector>
 #include <string>
 #include "cache.h"
-#include "base.h"
+#include "trace_file.h"
 
-using namespace base;
+using base::result;
 using std::vector;
 using std::string;
 
 typedef struct memory_access {
-    bool read_flag; //0:read 1:write
+    bool read_flag;
     unsigned int address;
 } memory_access;
 
-long long cache::total_address = 4294967296;    //cache_datas total count
+static unsigned int hex2int(const string& hex) {
+	unsigned int value = 0;
+	sscanf(hex.c_str(), "%x", &value);
+	return value;
+}
 
 memory_access cache::str2ma(string str) {
 	memory_access ma;
 	ma.read_flag = (str.find("read") != string::npos);
 	size_t address = str.find("0x");
-	ma.address = hex2int(str.substr(address, 10).c_str());
+	ma.address = hex2int(str.substr(address, 10));
 	return ma;
 }
 
-cache::cache(int cache_size, int block_size, MappingType type, WritePolicy policy) {
+cache::cache(int cache_size, int block_size, base::MappingType type, base::WritePolicy policy) {
 	this->cache_size = cache_size;
 	this->block_size = block_size;
 	this->mapping_type = type;
@@ -34,12 +38,12 @@ cache::cache(int cache_size, int block_size, MappingType type, WritePolicy polic
 	this->hit_counter = 0;
 	this->memory2cache = 0;
 	this->cache2memory = 0;
-	this->cmp_count = this->block_numbers_row = type != MT_FA ? type : cache_size / block_size;
-	this->cache_datas.resize(cache_size / block_size);
+	this->cmp_count = this->block_numbers_row = type != base::MT_FA ? type : cache_size / block_size;
+	this->datas.resize(cache_size / block_size);
 }
 
 cache::~cache() {
-	this->cache_datas.clear();
+	this->datas.clear();
 }
 
 void cache::process_access(string str) {
@@ -58,23 +62,23 @@ void cache::process_access(string str) {
 					this->hit_counter++;
 				} else {
 					this->miss_counter++;
+					this->memory2cache += block_size;
 				}
 			} else {
-				if (process_write_throught(ma.address)) {
+				if (process_write_through(ma.address)) {
 					this->hit_counter++;
-					this->cache2memory += 4;
 				} else {
 					this->miss_counter++;
-					this->cache2memory += 4;
+					this->memory2cache += block_size;
 				}
+				this->cache2memory += 4;
 			}
-			this->memory2cache += this->block_size;
 		}
 	} else {
-		if (this->write_back) {                       //process access is finished, cal the cache2memory in write back
-			for (int i = 0; i < cache_datas.size(); ++i) {
-				if (cache_datas[i].is_dirty)
-					this->cache2memory += this->block_size; //transfer dirty data from cache to memory
+		if (this->write_back) {                       //process access is finished, cal the cache2memory in write_back policy
+			for (int i = 0; i < datas.size(); ++i) {
+				if (datas[i].is_dirty)
+					this->cache2memory += block_size; //transfer dirty data from cache to memory
 			}
 		}
 	}
@@ -82,44 +86,43 @@ void cache::process_access(string str) {
 
 bool cache::process(unsigned int address, bool is_write) {
 	int rows = this->cache_size / (this->block_size * this->block_numbers_row);
-	long long address_range = total_address / rows;
-	int row = address / address_range;
-	int begin = row * this->block_numbers_row;
+	int row = (address / block_size) % rows;
+	int begin = row * block_numbers_row;
 	int max_index = begin;
 	int max_count = -1;
+	address = address / block_size ;
 	for (int i = 0; i < this->block_numbers_row; ++i) {
-		if (!cache_datas[begin + i].is_used) {      //search all and no hit, but has unused slot
-			cache_datas[begin + i].is_used = true;
-			cache_datas[begin + i].is_dirty = is_write;
-			cache_datas[begin + i].address = address;
-			cache_datas[begin + i].LRU_counter = 0;
+		if (!datas[begin + i].is_used) {      //miss, write in unused slot
+			datas[begin + i].is_used = true;
+			datas[begin + i].is_dirty = is_write;
+			datas[begin + i].address = address;
+			datas[begin + i].LRU_counter = 0;
 			return false;
 		} else {
-			int diff = address - cache_datas[begin + i].address;
-			if (diff < this->block_size && diff > 0) {          //hit
-				cache_datas[begin + i].LRU_counter = 0;
-				cache_datas[begin + i].is_dirty = is_write;
+			if (address == datas[begin + i].address) {          //hit
+				datas[begin + i].LRU_counter = 0;
+				datas[begin + i].is_dirty |= is_write;
 				i++;
-				while (i < this->block_numbers_row && cache_datas[begin + i].is_used) {
-					cache_datas[begin + i].LRU_counter++;
+				while (i < this->block_numbers_row && datas[begin + i].is_used) {
+					datas[begin + i].LRU_counter++;
 					i++;
 				}
 				return true;
 			} else {
-				if (cache_datas[begin + i].LRU_counter > max_count) {
-					max_count = cache_datas[begin + i].LRU_counter;
+				if (datas[begin + i].LRU_counter > max_count) {
+					max_count = datas[begin + i].LRU_counter;
 					max_index = begin + i;
 				}
-				cache_datas[begin + i].LRU_counter++;
+				datas[begin + i].LRU_counter++;
 			}
 		}
 	}
-	if (cache_datas[max_index].is_dirty && this->write_back) {
+	if (datas[max_index].is_dirty && this->write_back) {  //transfer dirty data from cache to memory
 		this->cache2memory += this->block_size;
 	}
-	cache_datas[max_index].LRU_counter = 0;
-	cache_datas[max_index].address = address;
-	cache_datas[max_index].is_dirty = is_write;
+	datas[max_index].LRU_counter = 0; //miss and no unused slot, replace the one which LRU_counter is max
+	datas[max_index].address = address;
+	datas[max_index].is_dirty = is_write;
 	return false;
 }
 
@@ -131,7 +134,7 @@ bool cache::process_write_back(unsigned int address) {
 	return process(address, true);
 }
 
-bool cache::process_write_throught(unsigned int address) {
+bool cache::process_write_through(unsigned int address) {
 	return process(address, true);
 }
 
@@ -140,8 +143,8 @@ result cache::generate_result() const {
 	r.cache_size = this->cache_size;
 	r.block_size = this->block_size;
 	r.mapping_type = this->mapping_type;
-	r.write_policy = this->write_back ? WP_WB : WP_WT;
-	r.hit_rate = this->hit_counter / (this->hit_counter + this->miss_counter);
+	r.write_policy = this->write_back ? base::WP_WB : base::WP_WT;
+	r.hit_rate = double(this->hit_counter) / (this->hit_counter + this->miss_counter);
 	r.mem2cache = this->memory2cache;
 	r.cache2mem = this->cache2memory;
 	r.cmp_count = this->cmp_count;
--- cache.h
+++ cache.h
@@ -9,7 +9,7 @@
 #include <vector>
 #include <string>
 struct memory_access;
-using namespace base;
+
 typedef struct cache_data{
     bool is_used;
 	bool is_dirty;
@@ -17,22 +17,23 @@ typedef struct cache_data{
 	int LRU_counter;
     cache_data(){is_used = false; is_dirty = false; address = 0; LRU_counter = 0;}
 }cache_data;
+
 using std::vector;
 using std::string;
+
 class cache{
 public:
-	explicit cache(int cache_size, int block_size, MappingType type, WritePolicy policy);
+	explicit cache(int cache_size, int block_size, base::MappingType type, base::WritePolicy policy);
 	~cache();
 	void process_access(string str);
-	result generate_result() const ;
+	base::result generate_result() const ;
 private:
 	static memory_access str2ma(string str);
     bool process(unsigned int address, bool is_write);
     bool process_read(unsigned int address);
 	bool process_write_back(unsigned int address);
-    bool process_write_throught(unsigned int address);
+    bool process_write_through(unsigned int address);
 private:
-	static long long total_address;
 	int cache_size;             //Cache size
 	int block_size;             //Block size
 	base::MappingType mapping_type; //Mapping type
@@ -43,7 +44,7 @@ private:
 	int memory2cache;           //Total bytes transferred from cache to memory
 	int cmp_count;              //Number of comparisons to identify a hit
     int block_numbers_row;      // blocks number in a row
-	vector<cache_data> cache_datas;
+	vector<cache_data> datas;
 };
 
 #endif //CACHE_SIMULATOR_CACHE_H
