49859c6994b03737e4174716c8eca9e3c13b7da7
Xuemin Sun <XueminSun@china-liantong.com>
2016-04-26 16:54:49 +0800

    fix bug in test

--- test_base.cpp
+++ test_base.cpp
@@ -14,19 +14,22 @@ void test_result2string(){
     result res;
     res.cache_size = 1024;
     res.block_size = 8;
-    res.mapping_type = MappingType::MT_DM;
-    res.write_policy = WritePolicy::WP_WT;
+    res.mapping_type = MT_DM;
+    res.write_policy = WP_WT;
     res.hit_rate = 0.16;
     res.mem2cache = 216;
     res.cache2mem = 96;
-    res.cmp_counter = 1;
+    res.cmp_count = 1;
     cout << result2string(res) << endl;
 }
 
 
 void test_hex2int(){
-    char* hex = "0x02000006";
+    const char* hex = "0x02000006";
     cout << hex2int(hex) << endl;
+
+    const char* hex1 = "0xffffffff";
+    cout << hex2int(hex1) << endl;
 }
 int main(){
     test_result2string();
