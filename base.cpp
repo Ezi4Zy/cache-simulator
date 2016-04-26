315c982666ad1f2ed0d0af006c84d4fb53852815
Xuemin Sun <XueminSun@china-liantong.com>
2016-04-26 16:51:00 +0800

    fix bug, convert result to string,the returned string add \n

--- base.cpp
+++ base.cpp
@@ -3,9 +3,7 @@
 //
 #include <stdio.h>
 #include "base.h"
-#include "cache.h"
-
-using namespace base;
+#include "trace_file.h"
 
 string base::result2string(const result &r) {
     string mapping_type;
@@ -36,18 +34,11 @@ string base::result2string(const result &r) {
         default:
             break;
     }
-    //1024	8	DM	WB	0.16	216	96	1
     char temp[100];
-    sprintf(temp, "%d\t%d\t%s\t%s\t%.2f\t%d\t%d\t%d", r.cache_size, r.block_size, mapping_type.c_str(),
+    sprintf(temp, "%d\t%d\t%s\t%s\t%.2f\t%d\t%d\t%d\n", r.cache_size, r.block_size, mapping_type.c_str(),
             write_policy.c_str(), r.hit_rate, r.mem2cache,
             r.cache2mem, r.cmp_count);
     return string(temp);
-}
-
-unsigned int base::hex2int(const char *hex) {
-    unsigned int value = 0;
-    sscanf(hex, "%x", &value);
-    return value;
 }
 
 
--- base.h
+++ base.h
@@ -8,7 +8,7 @@
 #include <string>
 
 using std::string;
-class cache;
+
 namespace base {
     enum MappingType {
         MT_DM = 1,
@@ -30,20 +30,14 @@ namespace base {
         double hit_rate;
         int mem2cache;
         int cache2mem;
-        int cmp_count;   //验证是否命中cache的比较次数。
-        // 映射方式为直接映射(MT_DM)，比较次数为1；映射方式为二路相联(MT_2W)，比较次数为2；
-        // 映射方式为四路相联(MT_4W)，比较次数为4；映射方式为全相联(MT_FA)，比较次数为cache_size/block_size；
+        int cmp_count;
     } result;
 
     /*
-     * 将result转为字符串输出
+     * transfer result to string
      */
-    string result2string(const result &r);
+    string result2string(const result& r);
 
-    /*
-    * 功能：将十六进制字符串转换为整型(int)数值
-    */
-    unsigned int hex2int(const char *hex);
 }
 
 #endif //CACHE_SIMULATOR_RESULT_H
